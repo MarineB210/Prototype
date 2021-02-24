fe4ff126a8df63df0e7c43b921a8f20cb1488b93
MarineB <marineop93@gmail.com>
2021-02-24 19:47:24 +0100

    Rajoute codes (check,str,moveUp,moveRight,addPawn)

--- brouillonMarine.py
+++ brouillonMarine.py
@@ -1,33 +1,60 @@
-def canMove(self):
+   
+#Idee pour representer le plateau. 
 
-    """ Verifie si le pion peut bouger à la ligne y et colonne x, retourne un booleen."""
+class Pawn:
 
-    if map[y][x] == 1: #1 --> mur , 0 ---> case vide
-        return False
-    else:
-        return True 
-        
-        
-#Idee pour representer la matrice. 
+#Ajoute les détails pour créer le tableau dans la class Pawn.
 
-class Plateau:
-
-#créer un plateau et méthodes permettant de l'actualiser.
-    def __init__(self): #Pour le moment je ne rajoute pas de paramètre car le plateau aura une taille fixe.
+    def __init__(self):                    #Pas de paramètre pour la matrice car elle aura une taille fixe.
        self.matrice = [[1,1,1,1,1,1,1],
-                       [1,0,0,0,0,0,1],    #matrice de taille 7x7
+                       [1,0,0,0,0,0,1],    #matrice de taille 7x7.
                        [1,0,0,0,0,0,1],
-                       [1,0,0,0,0,0,1],    #Créer une méthode qui va créer un pion dans la matrice ? Peut etre rajouter x et y 
+                       [1,0,0,0,0,0,1],  
                        [1,0,0,0,0,0,1],
                        [1,1,1,1,1,1,1]]
         
     def __str__(self):
-        plateau = str(self.matrice)
-        plateau = plateau.replace('[','').replace(',','').replace(']','')  #!!! Ajouter un retour à la ligne après chaque liste !!!
+        plateau = ''
+        for ligne in self.matrice: 
+            plateau += str(ligne).replace('[','').replace(',','').replace(']','') #Permet de représenter le tableau dans la console.
+            plateau += "\n"
         return plateau
         
+    def check(self):
+
+    """ Verifie si le pion peut bouger à la ligne y et colonne x, retourne un booleen."""
+
+    if self.matrice[y][x] == 1:         #1 --> mur , 0 ---> case vide, ne peut pas bouger lorsque c'est 1.
+        return False
+    else:
+        return True
+
+    def moveRight(self):                    #même code pour le moveLeft.
+        """..."""
+        if pass:
+            """..."""
+            self.matrice[self.y][self.x] = 0
+            self.x = x
+            self.matrice[self.y][self.x] = 2
+            """..."""
+
+    def moveUp(self):                       #même code pour le moveDown.
+        """..."""
+        if pass:
+           """..."""
+            self.matrice[self.y][self.x] = 0
+            self.y = y
+            self.matrice[self.y][self.x] = 2
+            """..."""
+        
+    def addPawn(self):
+        """ Ajoute le pion dans la matrice"""
+        
+        
+    
+    
                 
-r = Plateau()
+r = Pawn()
 print(r)                
         
 
