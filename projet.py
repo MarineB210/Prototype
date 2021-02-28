b8b286e030e6b05efa1629451cd9fd8ea7d2c076
Salma-brs <salmabourouiss10@gmail.com>
2021-02-28 12:28:40 +0100

    brouillon init et str

--- projet.py
+++ projet.py
@@ -1,8 +1,18 @@
 class Point:
     """represente un point"""
-    def __init__(self, x, y): #
+    def __init__(self, x, y): 
         self.x = x
         self.y = y
+        try:
+            self.x = int(x)
+            self.y = int(y)
+            
+        except ValueError:
+            print("Le pion ne peut être initialiser avec ces données")
+        if self.x == 1 or self.y == 1:
+            print("Le pion ne peut pas représenter le mur!")
+        if self.x >= 1 or self.y >= 1:
+            print("Le pion ne peut pas être représenter avec ces données")
 
     def __str__(self):
-        return '%g,%g' % (self.x, self.y)# pas certaine a refaire
+        return '%g,%g' % (self.x, self.y)
