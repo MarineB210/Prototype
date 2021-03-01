a3d261da903e39516cc4b34fc1f7510ba61a1cb2
Salma-brs <salmabourouiss10@gmail.com>
2021-03-01 14:41:53 +0100

    str et __init__

--- projet.py
+++ projet.py
@@ -1,17 +1,19 @@
 class Point:
     """represente un point"""
-    def __init__(self, x, y): 
+    def __init__(self, x=0, y=0): #les 0 sont des variables par defaut
        
         try:
-            self.x = int(x)
-            self.y = int(y)
-            
+           if self.x is not int(x) or self.y is not int(y):
+               raise ValueError ("Les coordonnées du pion ne sont pas acceptables")
         except ValueError:
             print("Le pion ne peut être initialiser avec ces données")
-        if self.matrice [x][y] == 1
-            print("Le pion ne peut pas représenter le mur!")
-        if self.matrice[x][y] >= 1
-            print("Le pion ne peut pas être représenter avec ces données")
+        if self.matrice [x][y] == 1:
+            print("Le pion ne peut pas représenter le mur")
+        if self.x >= 6 or self.y >=7:
+            print("Le pion ne peut pas être en dehors du plateau")
+        if self.x < 0 or self.y < 0:
+            print("Le pion ne peut être initialiser avec ces données")
+
         else:
             self.x = x
             self.y = y
