7b2d2b200db19b4ef3590eeea6e50fd0c5ed529e
MarineB <marineop93@gmail.com>
2021-03-01 21:26:50 +0100

    Fin du prototype

--- Prototype.py
+++ Prototype.py
@@ -11,7 +11,24 @@ class Pawn:
                         [1,0,0,0,0,0,1],
                         [1,1,1,1,1,1,1]]
 
-        
+        if type(x) != int and type(y) != int:
+            print("Le pion ne peut avoir ces données, le pion a pris (2,3) par défaut")     
+            self.x = 3
+            self.y = 2
+        elif self.matrice[x][y] == 1 :
+            self.x = 3
+            self.y = 2
+            print("Le pion ne peut pas représenter le mur, le pion a pris (2,3) par défaut")
+        elif x >= 5 or y >= 6:
+            self.x = 3
+            self.y = 2
+            print("Le pion ne peut pas être en dehors du plateau, le pion a pris (2,3) par défaut")
+        else:
+            self.x = x
+            self.y = y
+        self.matrice[self.x][self.y] = 2
+        print(self)
+ 
     def __str__(self):
         plateau = ''
         for ligne in self.matrice: 
@@ -19,68 +36,70 @@ class Pawn:
             plateau += "\n"
         return plateau
         
-    def check(self,x,y):
-        """ Verifie si le pion peut se placer à la position (x,y) (ligne,colonne), retourne un booleen."""
-        if self.matrice[x][y] == 1:         #1 --> mur , 0 ---> case vide, ne peut pas bouger lorsque c'est 1.
-            return False
-        else:
-            return True
    
     def MoveRight(self) :  
-        coordonateX = #coordonné dans la classe     #gérer le cas où le piont n'est pas initialisé.
-        coordonateY = #coordonné dans la classe  
-        verification = check(coordonateX,coordonateY+1) 
+        coordonateX = self.x       #coordonné dans la classe     #gérer le cas où le piont n'est pas initialisé.
+        coordonateY = self.y      #coordonné dans la classe  
+        if self.matrice[coordonateX][coordonateY+1] == 1:         #1 --> mur , 0 ---> case vide, ne peut pas bouger lorsque c'est 1.
+            verification = False
+        else:
+            verification = True
         if verification == True :
             self.matrice[coordonateX][coordonateY] = 0 
             self.matrice[coordonateX][coordonateY+1] = 2
-            self.y = coordonateY + 1    #Change coordonées du pion par les nouvelles obtenues après déplacement.
-            print(self) #Affiche le plateau après avoir bougé le pion.
-            return #coordonné du nouveau point du pion  
+            self.y = coordonateY + 1        #Change coordonées du pion par les nouvelles obtenues après déplacement.
+            print(self)     #Affiche le plateau après avoir bougé le pion. 
         else :
             message = "Impossible d'aller à droite!"
             print(self)
             return message
 
     def MoveLeft(self) :
-        coordonateX = #coordonné dans la classe     
-        coordonateY = #coordonné dans la classe
-        verification = check(coordonateX,coordonateY-1)
+        coordonateX = self.x     
+        coordonateY = self.y
+        if self.matrice[coordonateX][coordonateY-1] == 1:         #1 --> mur , 0 ---> case vide, ne peut pas bouger lorsque c'est 1.
+            verification = False
+        else:
+            verification = True
         if verification == True :
             self.matrice[coordonateX][coordonateY] = 0 
             self.matrice[coordonateX][coordonateY-1] = 2
             self.y = coordonateY - 1
             print(self) #Affiche le plateau après avoir bougé le pion.
-            return #coordonné du nouveau point du pion dans la classe
         else :
             message = "Impossible d'aller à gauche!"
             print(self)
             return message
 
     def MoveUp(self) :
-        coordonateX = #coordonné dans la classe     
-        coordonateY = #coordonné dans la classe
-        verification = check(coordonateX-1,coordonateY)
+        coordonateX = self.x    
+        coordonateY = self.y
+        if self.matrice[coordonateX-1][coordonateY] == 1:         #1 --> mur , 0 ---> case vide, ne peut pas bouger lorsque c'est 1.
+            verification = False
+        else:
+            verification = True
         if verification == True :
             self.matrice[coordonateX][coordonateY] = 0 
             self.matrice[coordonateX-1][coordonateY] = 2
             self.x = coordonateX - 1
-            print(self) #Affiche le plateau après avoir bougé le pion. 
-            return #coordonné du nouveau point du pion
+            print(self)     #Affiche le plateau après avoir bougé le pion. 
         else :
             message = "Impossible d'aller en haut!"
             print(self)
             return message
 
     def MoveDown(self) :                                
-        coordonateX = #coordonné dans la classe
-        coordonateY = #coordonné dans la classe
-        verification = check(coordonateX+1,coordonateY)
+        coordonateX = self.x
+        coordonateY = self.y
+        if self.matrice[coordonateX+1][coordonateY] == 1:         #1 --> mur , 0 ---> case vide, ne peut pas bouger lorsque c'est 1.
+            verification = False
+        else:
+            verification = True
         if verification == True :
             self.matrice[coordonateX][coordonateY] = 0 
             self.matrice[coordonateX+1][coordonateY] = 2
             self.x = coordonateX + 1 
             print(self) #Affiche le plateau après avoir bougé le pion.
-            return #coordonné du nouveau point du pion
         else :
             message = "Impossible d'aller en bas!"
             print(self)
