cd037b4639d91c662b584d19976c17d8952a8571
theotime-hucque <79421392+theotime-hucque@users.noreply.github.com>
2021-03-14 16:17:36 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,36 @@
-
+public class Player
+{
+	private int x ;
+	private int y ;
+	
+	public Player(int x , int y) 
+	{
+		this.x = x ;
+		this.y = y ;
+	}
+	
+	public Player()
+	{
+		this(1,1) ;
+	} 
+	
+	public int setX(int x)
+	{
+		return this.x = x ;
+	}
+	
+	public int getX()
+	{
+		return this.x ;
+	}
+	
+	public int setY(int y)
+	{
+		return this.y = y ;
+	} 
+	
+	public int getY()
+	{
+		return this.y ;
+	} 
+} 
