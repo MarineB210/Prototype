39c1622694af3fcfe7157da6d5aa13693d55b4f2
theotime-hucque <79421392+theotime-hucque@users.noreply.github.com>
2021-03-14 16:17:56 +0100

    Update Testeur.java

--- Testeur.java
+++ Testeur.java
@@ -1,1 +1,55 @@
+import java.util.Scanner ;
 
+public class Testeur
+{
+	public static void main(String[] args)
+	{
+		int[][] matrice ={{1,1,1,1,1,1,1},
+					{1,0,0,0,0,0,1},
+					{1,0,0,2,0,0,1},
+					{1,0,0,0,0,0,1},
+					{1,0,0,0,0,3,1},
+					{1,1,1,1,1,1,1}};
+		Player test = new Player(4,4);
+		System.out.println("Choisis une direction !");
+		System.out.println("1 => haut");
+		System.out.println("2 => droite");
+		System.out.println("3 => bas");
+		System.out.println("4 => gauche");
+		System.out.println("5 => quitter");
+		Scanner scan = new Scanner(System.in) ;
+		int number = scan.nextInt();
+		while(number != 5)
+		{
+			if(number > 5)
+			{
+				System.out.println("Choisis un chiffre entre 1 et 5 !");
+				scan.nextInt();
+			}
+			else if(number == 1)
+			{
+				Move.up(test,matrice);
+				number = scan.nextInt();
+			}
+			else if(number == 2)
+			{
+				Move.right(test,matrice);
+				number = scan.nextInt();
+			}
+			else if(number == 3)
+			{
+				Move.down(test,matrice);
+				number = scan.nextInt();
+			}
+			else if(number == 4)
+			{
+				Move.left(test,matrice);
+				number = scan.nextInt();
+			}
+			else
+			{
+				scan.close();
+			}
+		}
+	}
+}
